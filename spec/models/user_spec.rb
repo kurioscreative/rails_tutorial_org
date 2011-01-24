8372bbd9df565b29d8401b6ddf32e80e8f3d4241
Kurios Creative Design <glenn@kurioscreative.com>
2011-01-24 16:10:37 -0500

    Partial completion of chapterer 9, finished up until 9-4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -102,17 +102,17 @@ describe User do
     
     describe "authentication method" do
       it "should return nil on email/password mismatch" do
-        wrong_password_user = User.authentication(@attr[:email], "wrongpass")
+        wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
         wrong_password_user.should be_nil
       end
       
       it "should return nil for an email address with no user" do
-        nonexistent_user = User.authentication("bar@foo.com", @attr[:password])
+        nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
         nonexistent_user.should be_nil
       end
       
       it "should return the user on email/password match" do
-        matching_user = User.authentication(@attr[:email], @attr[:password])
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
         matching_user.should == @user
       end
       
